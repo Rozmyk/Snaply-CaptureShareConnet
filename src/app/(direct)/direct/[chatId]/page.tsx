d733734997546385913dc98e8cbca8643958ec06
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-03 12:49:12 +0100

    update Server-Side Rendering chatId

--- src/app/(direct)/direct/[chatId]/page.tsx
+++ src/app/(direct)/direct/[chatId]/page.tsx
@@ -1,14 +1,8 @@
 'use client'
-import { useEffect, useState } from 'react'
 import DirectPageContainer from '../../../../../components/Direct/DirectPageContainer/DirectPageContainer'
 import { useMediaQuery } from '@mantine/hooks'
-import DetailedSection from '../../../../../components/Direct/DirectPageContainer/DetailedSection/DetailedSection'
 import DirectMobile from '../../../../../components/Direct/DirectMobile/DirectMobile'
-import { Metadata } from 'next'
-// export const metadata: Metadata = {
-// 	title: 'Snaply • Direct',
-// 	description: 'Direct',
-// }
+
 export default function Page({ params }: { params: { chatId: string } }) {
 	const isSmallScreen = useMediaQuery('(max-width: 720px)')
 
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import ExploreContainer from '../../../components/ExplorePage/ExploreContainer/ExploreContainer'
 import Footer from '../../../components/Footer/Footer'
+
 const Page = () => {
 	return (
 		<>
