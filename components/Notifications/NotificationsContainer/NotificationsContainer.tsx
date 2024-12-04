91a5aa9dc0df3d453f4824d86b243698a7470d27
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-04 15:24:08 +0100

    remove suggestesforYou component

--- components/Notifications/NotificationsContainer/NotificationsContainer.tsx
+++ components/Notifications/NotificationsContainer/NotificationsContainer.tsx
@@ -1,13 +1,12 @@
 'use client'
 import { Container } from '@mantine/core'
-import SuggestedForYou from '../NotificationsContent/EmptyNotifications/SuggestedForYou/SuggestedForYou'
+
 import NotificationsContent from '../NotificationsContent/NotificationsContent'
 
 const NotificationsContainer = () => {
 	return (
 		<Container p='xs'>
 			<NotificationsContent withText={false} />
-			<SuggestedForYou />
 		</Container>
 	)
 }
