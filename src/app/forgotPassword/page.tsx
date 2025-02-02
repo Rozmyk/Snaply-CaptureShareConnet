1c4ff72ec844f26900889f1247b4bd57d3bcafa6
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-02 15:35:29 +0100

    Prevent forgot password page from rendering for authenticated users before redirecting

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { Flex, useMantineColorScheme, Box } from '@mantine/core'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import Navbar from '../../../components/ForgotPassword/Navbar/Navbar'
 import Footer from '../../../components/Footer/Footer'
@@ -8,9 +10,23 @@ const ForgotPassword = () => {
 	const { data: session, status } = useSession()
 	const { colorScheme } = useMantineColorScheme()
 	const dark = colorScheme === 'dark'
+	const router = useRouter()
+	const [isClient, setIsClient] = useState(false)
+
+	useEffect(() => {
+		setIsClient(true)
+
+		if (status === 'authenticated') {
+			router.push('/')
+		}
+	}, [status, router])
+
+	if (!isClient || status === 'loading' || status === 'authenticated') {
+		return null
+	}
 	return (
 		<Box sx={{ backgroundColor: dark ? 'black' : '#fefeff', minHeight: '100vh', height: '100vh' }}>
-			{status !== 'authenticated' && <Navbar />}
+			<Navbar />
 			<Flex h='100%' direction='column' justify='space-between'>
 				<Flex mt='75px' justify='center' align='center' w='100%'>
 					<ForgotPasswordForm />
