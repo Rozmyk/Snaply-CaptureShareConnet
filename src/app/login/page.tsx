50a3ecd7af649d6a8681830a27243a112640cbaf
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-02 13:36:47 +0100

    Prevent Login page from rendering for authenticated users before redirecting

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,28 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import LoginContainer from '../../../components/LoginPage/LoginContainer/LoginContainer'
-const login = () => {
+
+const Login = () => {
+	const { data: session, status } = useSession()
+	const router = useRouter()
+	const [isClient, setIsClient] = useState(false)
+
+	useEffect(() => {
+		setIsClient(true)
+
+		if (status === 'authenticated') {
+			router.push('/')
+		}
+	}, [status, router])
+
+	if (!isClient || status === 'loading' || status === 'authenticated') {
+		return null 
+	}
+
 	return <LoginContainer />
 }
 
-export default login
+export default Login
