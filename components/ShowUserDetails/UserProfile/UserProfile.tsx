1eb9e55cb17069d996fe71740591cd14743a174b
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-03 16:08:17 +0100

    Fix text color on dark and white mode

--- components/ShowUserDetails/UserProfile/UserProfile.tsx
+++ components/ShowUserDetails/UserProfile/UserProfile.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Flex, Text, Avatar } from '@mantine/core'
+import { Flex, Text, Avatar, useMantineColorScheme } from '@mantine/core'
+
 interface UserProfileProps {
 	username: string
 	name: string
 	image: string
 }
 const UserProfile = ({ username, name, image }: UserProfileProps) => {
+	const { colorScheme } = useMantineColorScheme()
+	const dark = colorScheme === 'dark'
 	return (
 		<Flex justify='flex-start' align='center' gap='15px'>
 			<Avatar size={50} radius='50%' alt={`${username} profile photo`} src={image} />
 			<Flex direction='column' gap={0}>
-				<Text m={0} p={0} fz='sm' fw={700}>
+				<Text m={0} p={0} fz='sm' fw={700} color={dark ? 'white' : 'black'}>
 					{username}
 				</Text>
 				<Text m={0} p={0} color='dark.2' fz='sm'>
--- components/ShowUserDetails/UserStats/UserStats.tsx
+++ components/ShowUserDetails/UserStats/UserStats.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Box, Flex, Text } from '@mantine/core'
+import { Box, Flex, Text, useMantineColorScheme } from '@mantine/core'
 interface UserStatsProps {
 	postLength: number
 	followersLength: number
 	followingLength: number
 }
 const UserStats = ({ postLength, followersLength, followingLength }: UserStatsProps) => {
+	const { colorScheme } = useMantineColorScheme()
+	const dark = colorScheme === 'dark'
 	return (
 		<Box
 			sx={{
@@ -16,19 +18,19 @@ const UserStats = ({ postLength, followersLength, followingLength }: UserStatsPr
 			mt='sm'
 			mb='sm'>
 			<Flex direction='column' justify='center' align='center'>
-				<Text fz='sm' fw={700}>
+				<Text fz='sm' fw={700} color={dark ? 'white' : 'black'}>
 					{postLength}
 				</Text>
 				<Text fz='sm'>posts</Text>
 			</Flex>
 			<Flex direction='column' justify='center' align='center'>
-				<Text fz='sm' fw={700}>
+				<Text fz='sm' fw={700} color={dark ? 'white' : 'black'}>
 					{followersLength}
 				</Text>
 				<Text fz='sm'>followers</Text>
 			</Flex>
 			<Flex direction='column' justify='center' align='center'>
-				<Text fz='sm' fw={700}>
+				<Text fz='sm' fw={700} color={dark ? 'white' : 'black'}>
 					{followingLength}
 				</Text>
 				<Text fz='sm'>following</Text>
