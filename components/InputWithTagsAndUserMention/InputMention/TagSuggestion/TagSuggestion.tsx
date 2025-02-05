d33b9733826feb118e02d62dfeb7491ef61b6039
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-05 17:19:19 +0100

    fix tag display

--- components/InputWithTagsAndUserMention/InputMention/TagSuggestion/TagSuggestion.tsx
+++ components/InputWithTagsAndUserMention/InputMention/TagSuggestion/TagSuggestion.tsx
@@ -13,7 +13,7 @@ const TagSuggestion = ({ suggestion, dark, postLength }: TagSuggestionProps) => 
 	return (
 		<Flex direction='column'>
 			<Text sx={{ maxWidth: 300, overflowWrap: 'break-word' }} color={dark ? 'gray.0' : 'gray.9'} fw={700} p={0} m={0}>
-				#{suggestion.display}
+				{suggestion.display}
 			</Text>
 			{postLength && <Text fz='xs'>{`Posts: ${postLength}`}</Text>}
 		</Flex>
