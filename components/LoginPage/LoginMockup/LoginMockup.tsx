274631ffd65e4932756b66a40f287e5adad60abe
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-10 16:29:46 +0100

    Update login mockup photo

--- components/LoginPage/LoginMockup/LoginMockup.tsx
+++ components/LoginPage/LoginMockup/LoginMockup.tsx
@@ -1,7 +1,35 @@
-import MockupPhone from '../../../public/mockupiphone.png'
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import Image from 'next/image'
+import firstMockup from '../../../public/firstMockup.webp'
+import secondMockup from '../../../public/secondMockup.webp'
+
 const LoginMockup = () => {
-	return <Image alt='Snaply app on phone' src={MockupPhone}></Image>
+	const [currentMockup, setCurrentMockup] = useState(firstMockup)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentMockup(prevMockup => (prevMockup === firstMockup ? secondMockup : firstMockup))
+		}, 5000)
+
+		return () => clearInterval(interval)
+	}, [])
+
+	return (
+		<div className='relative w-[400px] h-auto'>
+			<AnimatePresence mode='wait'>
+				<motion.div
+					key={currentMockup.src}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.5, ease: 'easeInOut' }}
+					className='absolute inset-0'>
+					<Image alt='Snaply app on phone' width={400} src={currentMockup} />
+				</motion.div>
+			</AnimatePresence>
+		</div>
+	)
 }
 
 export default LoginMockup
